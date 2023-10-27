5df38d9b07383456ab455c710226efca6b2950c4
Gabriel Del Rosso <gabrieldelrosso@gmail.com>
2023-10-27 20:19:15 -0300

    ex100

--- ex100.py
+++ ex100.py
@@ -1,14 +1,20 @@
-import random
-
-numeros = []
+from random import randint
 
 
 def sorteia(lista):
+    for cont in range(0, 5):
+        lista.append(randint(1, 10))
+
+
+def somapar(lista):
+    lista_pares =[]
     for c in range(0, 5):
-        sortear = random.randint(1, 10)
-        lista.append(sortear)
-    return lista
+        if lista[c] % 2 == 0:
+            lista_pares.append(lista[c])
+    print(sum(lista_pares))
 
 
+numeros = []
 sorteia(numeros)
 print(numeros)
+somapar(numeros)
