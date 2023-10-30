82391e6f03f43eb3de4129be0c9eb0e4cc0807d3
Gabriel Del Rosso <gabrieldelrosso@gmail.com>
2023-10-30 09:21:00 -0300

    Create ex101

---
+++ ex101.py
@@ -0,0 +1,21 @@
+import datetime
+
+
+def voto(ano):
+    """
+    > Função feita para determinar se um indivíduo deve votar ou não.
+    :param ano: ano de nascimento do individuo
+    :return:
+    """
+    ano_atual = datetime.datetime.now().year
+    votar = ano_atual - ano
+    if votar < 18:
+        print(f'Voto negado! Com {votar} anos é menor de idade.')
+    elif votar < 65:
+        print(f'Voto liberado! Com {votar} anos é obrigatório!')
+    else:
+        print(f'Voto opcional! Com {votar} é opcional o voto.')
+
+
+# Programa Principal
+voto(1940)
