d929b5966c670fe8afb177deea6d0fa7be7db25a
Gabriel Del Rosso <gabrieldelrosso@gmail.com>
2023-11-01 08:26:52 -0300

    Update ex003.py

--- ex003.py
+++ ex003.py
@@ -1,4 +1,4 @@
-var = float(input('Digite um número: '))
+n1 = float(input('Digite um número: '))
 n2 = float(input('Digite outro número: '))
 s = n1 + n2
-print('A soma ded {} e {} é {}.'.format(n1, n2, s))
+print('A soma de {} e {} é {}.'.format(n1, n2, s))
